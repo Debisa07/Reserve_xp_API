94ada7f0e1f99039f69ea801ff9393288a074625
unknown <firaoltulu5@gmail.com>
2023-12-22 16:08:45 +0300

    my first commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,90 +54,7 @@
 
 // module.exports = BookingController;
 
-
-const Car = require('../models/carModels.js');
 const Booking = require('../models/bookingModels.js');
-
-// GET all Cars
-const getAllCars = async (req, res) => {
-    try {
-        const cars = await Car.find({});
-        res.status(200).json(cars);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// GET a single car by ID
-const getCarById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const car = await Car.findById(id);
-
-        if (!car) {
-            return res.status(404).json({ message: "Car not found" });
-        }
-
-        res.status(200).json(car);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// CREATE a new car
-const createCar = async (req, res) => {
-    try {
-        const newCar = await Car.create(req.body);
-        res.status(201).json(newCar);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// UPDATE a car by ID
-const updateCarById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updatedCar = await Car.findByIdAndUpdate(id, req.body, { new: true });
-
-        if (!updatedCar) {
-            return res.status(404).json({ message: "Car not found" });
-        }
-
-        res.status(200).json(updatedCar);
-    } catch (error) {
-        console.log(error.message); // Add this line to log the error message
-        res.status(500).json({ message: error.message });
-    }
-};
-
-// DELETE a car by ID
-const deleteCarById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedCar = await Car.findByIdAndDelete(id);
-
-        if (!deletedCar) {
-            return res.status(404).json({ message: "Car not found" });
-        }
-
-        res.status(200).json({ message: "Car deleted successfully" });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
-    }
-};
-
-module.exports = {
-    getAllCars,
-    getCarById,
-    createCar,
-    updateCarById,
-    deleteCarById,
-};
 
 
 // GET all bookings
@@ -171,7 +88,15 @@ const getBookingById = async (req, res) => {
 // CREATE a new booking
 const createBooking = async (req, res) => {
     try {
-        const newBooking = await Booking.create(req.body);
+        const newobj = Object.assign({}, {
+            roomId: req.body.RoomId,
+            guestName: req.body.guestName,
+            checkInDate: req.body.checkInDate,
+            checkOutDate: req.body.checkOutDate,
+        });
+
+        const newBooking = await Booking.findOneAndUpdate(newobj);
+        console.log({ newBooking });
         res.status(201).json(newBooking);
     } catch (error) {
         console.log(error.message);
